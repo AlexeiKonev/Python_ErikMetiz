9d4aafe722353c28e482a5f7251e22909efe18c7
AlexeiKonev <alexkonev777@gmail.com>
2022-12-12 13:05:03 +0300

    pop

---
+++ 03_glava_Spiski/EmptyList/EmptyList/EmptyList.py
@@ -0,0 +1,18 @@
+#initiate empty list
+motorcycles = []
+#add element in list
+motorcycles.append('ducati')
+motorcycles.append('yamaha')
+motorcycles.append('honda')
+print(motorcycles)
+#use Insert method 
+motorcycles.insert(1,'harley')
+print(motorcycles)
+
+# use del for delete from list
+del motorcycles[0]
+print(motorcycles)
+
+popped_motorcycles = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycles)
